07bcdb01b3ca75f17ac4df4ded279913397661b1
Vas Tregubov <vastregubov@gmail.com>
2025-11-21 17:12:47 +0300

    feat(DTO): added film and order DTO's

--- backend/src/films/dto/films.dto.ts
+++ backend/src/films/dto/films.dto.ts
@@ -1,1 +1,13 @@
-//TODO описать DTO для запросов к /films
+export class FilmDto {
+  id: number;
+  rating: number;
+  director: string;
+  tags: string;
+  description: string;
+}
+
+export class ScheduleDto {
+  id: string;
+  day: string;
+  time: string;
+}
